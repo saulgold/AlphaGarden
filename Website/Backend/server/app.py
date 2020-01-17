5cca39baad7313101bb2e5be10d1b34c67d6bd8b
William <william@Johns-MacBook-Pro.local>
2020-01-16 22:27:49 -0800

    Get one image setup

---
+++ Website/Backend/server/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import os
+import pyrebase
+
+app = Flask(__name__)
+
+config = {
+  "apiKey": os.environ.get("FIREBASE_API_KEY"),
+  "authDomain": "alphagarden-3d4f7.firebaseapp.com",
+  "databaseURL": "https://alphagarden-3d4f7.firebaseio.com",
+  "projectId": "alphagarden-3d4f7",
+  "storageBucket": "alphagarden-3d4f7.appspot.com",
+  "serviceAccount": "alphagarden-3d4f7-firebase-adminsdk-tll4n-2648401a5b.json",
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+@app.route('/overhead', methods=['GET'])
+def overhead():
+  return storage.child("01_15_2020_cal.bmp").get_url(None)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
