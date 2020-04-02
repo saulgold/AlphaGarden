dbe9392e47e7e659dab84a42e573264f99dac444
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2020-04-02 10:38:33 +0800

    add function for invasive species

--- Simulator_v2/simulatorv2/plant_type.py
+++ Simulator_v2/simulatorv2/plant_type.py
@@ -115,3 +115,41 @@ class PlantType:
 
         self.non_plant_centers = [c for c in coords if in_bounds(c[0], c[1])]
         return plants
+
+    def get_same_plants_with_invasive_species(self, seed, rows, cols, sector_rows, sector_cols):
+        self.plant_in_bounds = 0
+        self.plant_centers = []
+        self.non_plant_centers = []
+        
+        np.random.seed(seed)
+        plants = []
+        sector_rows_half = sector_rows // 2
+        sector_cols_half = sector_cols // 2
+        
+        def in_bounds(r, c):
+            return r > sector_rows_half and r < rows - sector_rows_half and c > sector_cols_half and c < cols - sector_cols_half
+        
+        coords = [(r, c) for c in range(cols) for r in range(rows)]
+        np.random.shuffle(coords)
+        num_invasive = np.random.randint(0, NUM_PLANTS)
+
+        # plant invasive plantss
+        for i in range(num_invasive):
+            name, plant = self.plant_types[0]
+            coord = coords.pop(0)
+            r, c = coord[0], coord[1]
+            plants.extend([Plant(r, c, c1=plant['c1'], growth_time=plant['growth_time'],
+                                    color=plant['color'], plant_type=name, stopping_color=plant['stopping_color'], color_step=plant['color_step'])])
+            self.plant_in_bounds += 1
+            self.plant_centers.append(tuple((r, c)))
+
+        for i in range(NUM_PLANTS - num_invasive):
+            name, plant = self.plant_types[np.random.randint(1, self.num_plant_types)]  # random plant type excluding invasive
+            coord = coords.pop(0)
+            r, c = coord[0], coord[1]
+            plants.extend([Plant(r, c, c1=plant['c1'], growth_time=plant['growth_time'],
+                                    color=plant['color'], plant_type=name, stopping_color=plant['stopping_color'], color_step=plant['color_step'])])
+            self.plant_in_bounds += 1
+            self.plant_centers.append(tuple((r, c)))
+        self.non_plant_centers = [c for c in coords if in_bounds(c[0], c[1])]
+        return plants
